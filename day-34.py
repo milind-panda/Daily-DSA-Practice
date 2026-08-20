c78260a6ee9f98ee263c6e78c5a4ef3dc2400d14
Milind Panda <ayushpanda74@gmail.com>
2026-08-20 20:18:34 +0530

    Create day-34.py

---
+++ day-34.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseArray(self, arr):
+        # code here
+        
+        left = 0
+        right = len(arr) - 1
+
+        while left < right:
+            # Pythonic swap syntax
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
